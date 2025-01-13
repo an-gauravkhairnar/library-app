3f3839aef97c6633c0189ad8dd77f61fd7bee410
pankaj <pankaj@mantrax.io>
2025-01-13 16:39:33 +0530

    Updated css

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,25 +1,26 @@
-import { IonButton, IonCol, IonContent, IonRow, IonText } from '@ionic/react';
-import { auth, firebase } from '../../config/firebase';
-import userService from '../../services/user.service';
-import { useHistory } from 'react-router';
-import { useLoadingAndToast } from '../../hooks/useLoadingAndToast';
-import { RESGISTRATION,NOTACEESS} from '../../constants/constants'
-import { setValue } from '../../services/storage.service'
-import '../Login/Login.scss';
+import { IonButton, IonCol, IonContent, IonRow, IonText } from "@ionic/react";
+import { auth, firebase } from "../../config/firebase";
+import userService from "../../services/user.service";
+import { useHistory } from "react-router";
+import { useLoadingAndToast } from "../../hooks/useLoadingAndToast";
+import { RESGISTRATION, NOTACEESS } from "../../constants/constants";
+import { setValue } from "../../services/storage.service";
+import "../Login/Login.scss";
 
 const LoginPage: React.FC = () => {
-
   const history = useHistory();
   const { showToast, presentLoader, dismissLoader } = useLoadingAndToast();
-  const googleLogin = (async () => {
+  const googleLogin = async () => {
     try {
       presentLoader();
       const provider = new firebase.auth.GoogleAuthProvider();
       const result = await auth.signInWithPopup(provider);
       const user: any = result.additionalUserInfo?.profile;
-      if (user?.email.includes('application')) {
+      if (user?.email.includes("application")) {
         const isPresent = await userService.getUser(user?.email);
-        isPresent.length === 0 ? afterGoogleLoginSuccess(result, user) : rediractAdmin(user);
+        isPresent.length === 0
+          ? afterGoogleLoginSuccess(result, user)
+          : rediractAdmin(user);
       } else {
         dismissLoader();
         showToast(NOTACEESS);
@@ -28,11 +29,13 @@ const LoginPage: React.FC = () => {
       dismissLoader();
       showToast(NOTACEESS);
     }
-  })
+  };
 
   const afterGoogleLoginSuccess = async (result: any, user: any) => {
     try {
-      const credentials = firebase.auth.GoogleAuthProvider.credential(result.credential.idToken);
+      const credentials = firebase.auth.GoogleAuthProvider.credential(
+        result.credential.idToken
+      );
       const googleSignInResult = await auth.signInWithCredential(credentials);
       if (googleSignInResult) {
         user.role = 1;
@@ -45,24 +48,30 @@ const LoginPage: React.FC = () => {
     }
   };
 
-  const rediractAdmin = (data:any) =>{
+  const rediractAdmin = (data: any) => {
     const user = {
       name: data.name,
-      email:data.email
-    }
-    setValue("User",JSON.stringify(user));
+      email: data.email,
+    };
+    setValue("User", JSON.stringify(user));
     dismissLoader();
-    history.push('admin');
-  }
+    history.push("admin");
+  };
 
   return (
     <IonContent>
       <IonRow>
-        <IonCol size='12' className='ion-text-center'>
+        <IonCol size="12" className="ion-text-center">
           <IonText>Nexus Library</IonText>
         </IonCol>
       </IonRow>
-      <IonButton expand="block" onClick={() => googleLogin()}>Google SignUp</IonButton>
+      <IonButton
+        expand="block"
+        onClick={() => googleLogin()}
+        className="ion-margin"
+      >
+        Google SignUp
+      </IonButton>
     </IonContent>
   );
 };
